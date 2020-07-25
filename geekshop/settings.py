09aee522880648aae6e7ee8169bfb561f8ba6e6f
alex vydrin <tapat@mail.ru>
2020-07-26 00:23:23 +0300

    add_ALLOWED_HOSTS

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'wag($&!r%xw_x-731tk5-#y&7_5do(om&bg=0b%!y0^&m836m%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'alexgeekshop.pythonanywhere.com']
 
 # Application definition
 
