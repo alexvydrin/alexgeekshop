e0bec39d1cea92bf844a254a5799a73ff1b0c07b
alex vydrin <tapat@mail.ru>
2020-09-06 19:03:10 +0300

    homework_12_1

--- authapp/models.py
+++ authapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils.timezone import now
-from datetime import timedelta
+from django.conf import settings
+from datetime import datetime, timedelta
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import pytz
 
 
 class ShopUser(AbstractUser):
@@ -11,13 +12,14 @@ class ShopUser(AbstractUser):
     age = models.PositiveIntegerField(verbose_name='возраст', default=18)
 
     activation_key = models.CharField(max_length=128, blank=True)
-    activation_key_expires = models.DateTimeField(default=(now() + timedelta(hours=48)))
+    activation_key_expires = models.DateTimeField(default=(datetime.now() + timedelta(hours=48)))
 
     def is_activation_key_expired(self):
-        if now() <= self.activation_key_expires:
-            return False
-        else:
-            return True
+        # if now() <= self.activation_key_expires:
+        #    return False
+        # else:
+        #    return True
+        return datetime.now(pytz.timezone(settings.TIME_ZONE)) > self.activation_key_expires
 
 
 class ShopUserProfile(models.Model):
